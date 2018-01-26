02d6e4df38a3a354d340854711aeb76daab20430
Unknown <tommaso4.bianchi@mail.polimi.it>
2018-01-26 23:39:55 +0100

    Working on waypoints

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+
+    public Waypoint[] neighbours;
+
+    private Color color = Color.black;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawSphere(transform.position, 200);
+        for (int i = 0; neighbours != null && i < neighbours.Length; i++)
+        {
+            if(neighbours[i] != null)
+                Gizmos.DrawLine(transform.position, neighbours[i].transform.position);
+        }
+    }
+}
