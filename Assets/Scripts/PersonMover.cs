892723278625e9267ba1ce516ab3c2d810ff5a68
Unknown <tommaso4.bianchi@mail.polimi.it>
2018-01-27 01:44:16 +0100

    Added a little bit of randomness to PlayerMover's targets

--- Assets/Scripts/PersonMover.cs
+++ Assets/Scripts/PersonMover.cs
@@ -10,6 +10,7 @@ public class PersonMover : MonoBehaviour {
     private float speed = 1f;
     private float turningSpeed = 500f;
     private Waypoint currentWaypoint;
+    private Vector3 currentWaypointTargetPosition;
     
 	void Start ()
     {
@@ -18,12 +19,12 @@ public class PersonMover : MonoBehaviour {
 
     void Update ()
     {
-        if ((transform.position - currentWaypoint.transform.position).sqrMagnitude < 0.1f * 0.1f)
+        if ((transform.position - currentWaypointTargetPosition).sqrMagnitude < 0.1f * 0.1f)
         {
             UpdateWaypoint();
         }
 
-        Vector3 dir = (currentWaypoint.transform.position - transform.position).normalized;
+        Vector3 dir = (currentWaypointTargetPosition - transform.position).normalized;
         float angle = Vector2.SignedAngle(transform.right, dir);
         if (angle > 0)
         {
@@ -39,7 +40,14 @@ public class PersonMover : MonoBehaviour {
     private void UpdateWaypoint()
     {
         currentWaypoint = currentWaypoint.neighbours[Random.Range(0, currentWaypoint.neighbours.Length)];
+        currentWaypointTargetPosition = currentWaypoint.transform.position + OnUnitCircle() * 0.25f;
         speed = GaussianDistribution.Generate(speedMean, speedVariance);
+    }
+
+    private Vector3 OnUnitCircle()
+    {
+        float randomAngle = Random.Range(0, 360.0f);
+        return new Vector3(Mathf.Cos(randomAngle), Mathf.Sin(randomAngle), 0);
     }
 
     public void SetWaypoint(Waypoint waypoint)
