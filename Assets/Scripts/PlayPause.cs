61cdc73f4b0f53f2e575a7c4588cb2a0699b5a9e
Giacomo Franchini <jack-f@hotmail.it>
2018-01-28 12:39:35 +0100

    Play/pause Iccons and trashBin icon

---
+++ Assets/Scripts/PlayPause.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayPause : MonoBehaviour {
+
+    public Sprite Play;
+    public Sprite Pause;
+
+    private bool isPaused;
+
+    private void Start()
+    {
+        isPaused = false;
+    }
+
+    public void ChangeIcon ()
+    {
+        if (isPaused)
+        {
+            gameObject.transform.GetComponent<Image>().sprite = Play;
+            isPaused = false;
+        }
+        else
+        {
+            gameObject.transform.GetComponent<Image>().sprite = Pause;
+            isPaused = true;
+        }
+    }
+}
