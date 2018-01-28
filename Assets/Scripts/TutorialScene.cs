39a736a7b4de1fa412acb1e0f50aaab73ac17740
Giacomo Franchini <jack-f@hotmail.it>
2018-01-28 17:14:17 +0100

    tutorial scene final

--- Assets/Scripts/TutorialScene.cs
+++ Assets/Scripts/TutorialScene.cs
@@ -9,20 +9,21 @@ public class TutorialScene : MonoBehaviour {
     public GameObject firstPage;
     public float fisrtTimer;
     public float secondTimer;
+    private float start;
 	// Use this for initialization
 	void Start () {
-		
+        start = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time >= fisrtTimer)
+		if (Time.time >= start+fisrtTimer)
         {
             firstPage.SetActive(false);
             secondPage.SetActive(true);
         }
         
-        if (Time.time >= secondTimer)
+        if (Time.time >= start+secondTimer+fisrtTimer)
         {
             SceneManager.LoadScene(3);
         }
