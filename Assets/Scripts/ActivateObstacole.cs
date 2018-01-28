d09155bc3e64546166808f5c1a75752e03e4fcc6
Unknown <tommaso4.bianchi@mail.polimi.it>
2018-01-28 09:27:10 +0100

    Created ActivateObstacle script

---
+++ Assets/Scripts/ActivateObstacole.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObstacole : MonoBehaviour {
+
+    public Sprite alternativeSprite;
+    public SpriteRenderer spriteRenderer;
+    public Color disabledColor;
+
+    private Sprite normalSprite;
+
+    void Start()
+    {
+        if (spriteRenderer) normalSprite = spriteRenderer.sprite;
+    }
+    
+	public void Activate()
+    {
+        if (alternativeSprite && spriteRenderer)
+        {
+            spriteRenderer.sprite = alternativeSprite;
+        }
+        else
+        {
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+    public void Deactivate()
+    {
+        if (alternativeSprite && spriteRenderer)
+        {
+            spriteRenderer.sprite = normalSprite;
+        }
+        else
+        {
+            spriteRenderer.color = disabledColor;
+        }
+    }
+}
