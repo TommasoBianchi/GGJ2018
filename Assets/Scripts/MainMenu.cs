32221b2ef119c80b914d3d539f5f5570129f9285
Unknown <tommaso4.bianchi@mail.polimi.it>
2018-01-28 14:45:34 +0100

    MainMenu  + Audio theme

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject mainScreenPanel;
+    public GameObject creditsPanel;
+
+    void Start ()
+    {
+		
+	}
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        creditsPanel.SetActive(true);
+        mainScreenPanel.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMainScreen()
+    {
+        creditsPanel.SetActive(false);
+        mainScreenPanel.SetActive(true);
+    }
+}
