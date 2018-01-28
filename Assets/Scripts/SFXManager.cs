caedf8db4c8f3d247791ac0ee7362e50611f2dc1
Unknown <tommaso4.bianchi@mail.polimi.it>
2018-01-28 15:03:16 +0100

    Button click sfx

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour {
+
+    public AudioClip[] samples;
+
+    public SFXManager Instance { get; private set; }
+
+    void Start()
+    {
+        Instance = this;
+    }
+
+    public void PlaySample(int index)
+    {
+        if (index < samples.Length)
+        {
+            AudioSource.PlayClipAtPoint(samples[index], Vector3.zero);
+        }
+    }
+}
