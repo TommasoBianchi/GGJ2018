634baa85c7c2770edccaee5c41321e68f90f898d
Unknown <tommaso4.bianchi@mail.polimi.it>
2018-01-28 06:35:01 +0100

    Created GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager {
+
+    public enum PersonType
+    {
+        Gamer,
+        Sportman,
+        Girl
+    }
+
+	public static int killCounter { get; private set; }
+    private static Dictionary<PersonType, int> killPoints;
+
+    static GameManager()
+    {
+        killCounter = 0;
+        killPoints = new Dictionary<PersonType, int>();
+        killPoints.Add(PersonType.Gamer, 0);
+        killPoints.Add(PersonType.Sportman, 0);
+        killPoints.Add(PersonType.Girl, 0);
+    }
+
+    public static int GetKillPoints(PersonType personType)
+    {
+        return killPoints[personType];
+    }
+
+    public static void RegisterKill(PersonType personType)
+    {
+        killCounter++;
+        killPoints[personType] += 1;
+    }
+
+    public static void ConsumeKillPoints(PersonType personType, int amount)
+    {
+        if (killPoints[personType] < amount)
+            Debug.LogError("You cannot use " + amount + " kill points for " + personType + " because you only have " + killPoints[personType]);
+        killPoints[personType] -= amount;
+    }
+}
